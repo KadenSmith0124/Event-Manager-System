4a0be508789c87b7f4f832d488bd6519408fefe8
Jeremy Murray <S547310@nwmissouri.edu>
2025-02-11 14:09:16 -0600

    test

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFcAzIbkyu33GuRAS_oYtc6IUlmxqFEdM",
+  authDomain: "evnt-320a0.firebaseapp.com",
+  projectId: "evnt-320a0",
+  storageBucket: "evnt-320a0.firebasestorage.app",
+  messagingSenderId: "146980889296",
+  appId: "1:146980889296:web:85312e3d4a0c3f396d74d5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
